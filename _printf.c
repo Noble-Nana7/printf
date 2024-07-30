de0af05a1bd0c582f1c2a5d7ee1622b26e49a5b5
Noble-Nana7 <nnaemekanoble7@gmail.com>
2024-07-29 19:49:40 -0700

    ANOTHER TRY

--- _printf.c
+++ _printf.c
@@ -1,5 +1,9 @@
 #include "main.h"
-
+/**
+ * _printf - My custom implementation of printf
+ * @format: Format string containing the text and format specifiers
+ * Return: Number of characters printed (excluding null byte)
+ */
 int _printf(const char *format, ...)
 {
 	va_list args;
@@ -17,24 +21,19 @@ int _printf(const char *format, ...)
 		{
 			++ptr;
 			if (*ptr == 'c')
-			{
 				c = (char) va_arg(args, int);
 				printed_chars += print_char(c);
-			}
 			else if (*ptr == 's')
-			{
 				str = va_arg(args, const char *);
 				printed_chars += print_string(str);
-			}
 			else if (*ptr == '%')
-			{
 				printed_chars += write(1, "%", 1);
-			}
+			else
+				printed_chars += write(1, "%", 1);
+				printed_chars += write(1, ptr, 1);
 		}
 		else
-		{
 			printed_chars += write(1, ptr, 1);
-		}
 	}
 	va_end(args);
 	return (printed_chars);
